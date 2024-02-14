797f60e36d059e2b0cc60dc257375d8e8e778962
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-02-14 13:05:48 -0600

    Butter Chicken recipe added

---
+++ BE/db/recipes.js
@@ -0,0 +1,59 @@
+const recipes = [
+    {
+        title: "Butter Chicken",
+        ingredients: [
+            "1.5 lbs Chicken Thighs", 
+            "1 cup full-fat yogurt", 
+            "1 tbsp grated or crushed garlic", 
+            "1 tbsp grated ginger", 
+            "1 tsp turmeric powder",
+            "1 tsp ground cumin",
+            "1 tsp ground coriander",
+            "1 tsp garam masala",
+            "Salt to taste",
+            "1 tsp oil or ghee",
+            "2 tbsp butter",
+            "1 large onion, finely diced",
+            "1 cup tomato puree",
+            "1 cup heavy cream",
+            "Fresh cilantro, for garnish"
+        ],
+        content: [
+            "Mix yogurt, ginger, garlic, and spices (reserve half of garam masala) to a large bowl and mix well. Add in the chicken and marinate for at least 30min up to a day.",
+            "In a large pan or dutch oven, add your oil over medium heat. Cook the chicken until well browned. It does not need to be cooked through at this stage.",
+            "Set the chicken aside and add the butter to the pan. Once melted and bubling, add the choped onion to the pan.",
+            "Once the onions are fully softened, add in the tomato purre and cook on high until the oil seperated and the tomato starts to brown.",
+            "Add the chicken back to the pan and stir to combine the mixture.",
+            "Pour in the heavy cream and cook on medium low heat for 10-15 minutes or until the chicken is tender.",
+            "Add in your remaining garam masala and any additional salt to taste. Adjust the consistency of the gravy with water only if absolutely neccessary.",
+            "Serve with naan and rice. Garnish with your chopped cilantro"
+        ],
+        imgUrl: "https://www.foodiesfeed.com/wp-content/uploads/2023/03/close-up-of-butter-chicken-indian-dish.jpg"
+    },
+    {
+        title: "",
+        ingredients: [],
+        content: [],
+        imgUrl: ""
+    },
+    {
+        title: "",
+        ingredients: [],
+        content: [],
+        imgUrl: ""
+    },
+    {
+        title: "",
+        ingredients: [],
+        content: [],
+        imgUrl: ""
+    },
+    {
+        title: "",
+        ingredients: [],
+        content: [],
+        imgUrl: ""
+    }
+]
+
+module.exports(recipes)
