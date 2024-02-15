359da66dd744186bda09d0f7d0bfca534f8053ee
Larry Reaux <larryreaux@gmail.com>
2024-02-14 19:19:46 -0600

    26-come-up-with-recipe-content finished
    Adding 5 recipes to a new file in the db folder.
    Came up with function to parse arrays.
    Made a folder for locally storing photos temp.

--- BE/db/recipes.js
+++ BE/db/recipes.js
@@ -1,85 +1,169 @@
 const recipes = [
-    {
-        title: "Butter Chicken",
-        ingredients: [
-            "Time: 1 hour",
-            "1.5 lbs Chicken Thighs", 
-            "1 cup full-fat yogurt", 
-            "1 tbsp grated or crushed garlic", 
-            "1 tbsp grated ginger", 
-            "1 tsp turmeric powder",
-            "1 tsp ground cumin",
-            "1 tsp ground coriander",
-            "1 tsp garam masala",
-            "Salt to taste",
-            "1 tsp oil or ghee",
-            "2 tbsp butter",
-            "1 large onion, finely diced",
-            "1 cup tomato puree",
-            "1 cup heavy cream",
-            "Fresh cilantro, for garnish"
-        ],
-        content: [
-            "Mix yogurt, ginger, garlic, and spices (reserve half of garam masala) to a large bowl and mix well. Add in the chicken and marinate for at least 30min up to a day.",
-            "In a large pan or dutch oven, add your oil over medium heat. Cook the chicken until well browned. It does not need to be cooked through at this stage.",
-            "Set the chicken aside and add the butter to the pan. Once melted and bubling, add the choped onion to the pan.",
-            "Once the onions are fully softened, add in the tomato purre and cook on high until the oil seperated and the tomato starts to brown.",
-            "Add the chicken back to the pan and stir to combine the mixture.",
-            "Pour in the heavy cream and cook on medium low heat for 10-15 minutes or until the chicken is tender.",
-            "Add in your remaining garam masala and any additional salt to taste. Adjust the consistency of the gravy with water only if absolutely neccessary.",
-            "Serve with naan and rice. Garnish with your chopped cilantro"
-        ],
-        imgUrl: "https://www.foodiesfeed.com/wp-content/uploads/2023/03/close-up-of-butter-chicken-indian-dish.jpg"
-    },
-    {
-        title: "Chicken and Sausage Gumbo",
-        ingredients: [
-            "1 Onion, finely diced",
-            "1 Bell Pepper, finely diced",
-            "3 Stalks Celery, finely diced",
-            "3-5 Cloves Garlic finely minced",
-            "1 lbs Andoullie Sausage",
-            "2 lbs Boneless Skinless Chicken Thighs",
-            "1-2 quarts Chicken Stock",
-            "2 tbsp flour",
-            "2 tbsp vegetable oil",
-            "Tony Chachere's to taste",
-            "Cayenne Pepper to taste",
-            "Salt to taste",
-            "Fresh Ground Black Pepper to taste",
-            "MSG to taste"
-        ],
-        content: [
-            "Time: 4-5 hours",
-            "Place your flour and oil in a heavy bottom pot or dutch oven that can hold around 4 quarts.",
-            "Constantly stir your roux (flour/oil mixture) over medium heat until it is as dark as possible without being burnt. You should aim for what's called a \"red roux\" which is just past a dark dirt brown. This step takes practice to get right.",
-            "While your roux is going, I like to brown my andouille and chicken in a separate pan. You'll want to go ahead and slice the sasauge, but leave the chicken thighs whole for now. You can alternatively brown these before you start the roux in the same pot/dutch oven, but you have to be careful not to burn the fond when making the roux.",
-            "When roux is the desired color, carefully add your holy trinity (diced vegetables) and garlic and stir vigorously. Alternatively a frozen store mix works fine. It\'s for flavor and thickening not for texture of the vegetables.",
-            "After 2-3 minutes, or once you notice the vegetables soften slowly add the chicken stock around a cup at a time, allowing the mixture to come to a boil between additions. Quick note on stock, homemade is best, but Better than Bouillon Roasted Chicken Base works better than any carton stock. Just reconstitute according to package directions.",
-            "Chop the chicken thighs and add them along with the sausage to your gumbo. Leave on medium low heat for at least thirty minutes, but preferably a few hours. DO NOT let the gumbo get above a bare simer or you will end up with rubbery, dry sausage.",
-            "Season to taste. I like to start with the Tony's until it is most of the way there, then make small adjustments to heat and salinity with the others. If your nose dosen't run, it's not spicy enough.",
-            "Serve with rice and plenty of hot sauce. Garlic bread is also a great addition."
-        ],
-        imgUrl: ""
-    },
-    {
-        title: "",
-        ingredients: [],
-        content: [],
-        imgUrl: ""
-    },
-    {
-        title: "",
-        ingredients: [],
-        content: [],
-        imgUrl: ""
-    },
-    {
-        title: "",
-        ingredients: [],
-        content: [],
-        imgUrl: ""
-    }
-]
+  {
+    title: "Butter Chicken",
+    estimatedTime: "1 hour",
+    ingredients: [
+      "1.5 lbs Chicken Thighs",
+      "1 cup Full-Fat Yogurt",
+      "1 tbsp Grated or Crushed Garlic",
+      "1 tbsp Grated Ginger",
+      "1 tsp Turmeric Powder",
+      "1 tsp Ground Cumin",
+      "1 tsp Ground Coriander",
+      "1 tsp Garam Masala",
+      "Salt to Taste",
+      "1 tsp Oil or Ghee",
+      "2 tbsp Butter",
+      "1 Large Onion, Finely Diced",
+      "1 cup Tomato Puree",
+      "1 cup Heavy Cream",
+      "Fresh Cilantro, for Garnish",
+    ],
+    procedure: [
+      "Mix yogurt, ginger, garlic, and spices (reserve half of garam masala) to a large bowl and mix well. Add in the chicken and marinate for at least 30min up to a day.",
+      "In a large pan or dutch oven, add your oil over medium heat. Cook the chicken until well browned. It does not need to be cooked through at this stage.",
+      "Set the chicken aside and add the butter to the pan. Once melted and bubbling, add the chopped onion to the pan.",
+      "Once the onions are fully softened, add in the tomato puree and cook on high until the oil separated and the tomato starts to brown.",
+      "Add the chicken back to the pan and stir to combine the mixture.",
+      "Pour in the heavy cream and cook on medium low heat for 10-15 minutes or until the chicken is tender.",
+      "Add in your remaining garam masala and any additional salt to taste. Adjust the consistency of the gravy with water only if absolutely necessary.",
+      "Serve with naan and rice. Garnish with your chopped cilantro.",
+    ],
+    imgUrl:
+      "https://www.foodiesfeed.com/wp-content/uploads/2023/03/close-up-of-butter-chicken-indian-dish.jpg",
+    notes: [
+      "I like to make the naan too as its a very easy and approachable flat bread to make at home.",
+    ],
+  },
+  {
+    title: "Chicken and Sausage Gumbo",
+    estimatedTime: "4-5 hours",
+    ingredients: [
+      "1 Onion, Finely Diced",
+      "1 Bell Pepper, Finely Diced",
+      "3 Stalks Celery, Finely Diced",
+      "3-5 Cloves Garlic Finely Minced",
+      "1 lbs Andoullie Sausage",
+      "2 lbs Boneless Skinless Chicken Thighs",
+      "1-2 quarts Chicken Stock",
+      "2 tbsp Flour",
+      "2 tbsp Vegetable Oil",
+      "Tony Chachere's to Taste",
+      "Cayenne Pepper to Taste",
+      "Salt to Taste",
+      "Fresh Ground Black Pepper to Taste",
+      "MSG to Taste",
+    ],
+    procedure: [
+      "Place your flour and oil in a heavy bottom pot or dutch oven that can hold around 4 quarts.",
+      "Constantly stir your roux (flour/oil mixture) over medium heat until it is as dark as possible without being burnt. See note 1.",
+      "While your roux is going, I like to brown my andouille and chicken in a separate pan. You'll want to go ahead and slice the sausage, but leave the chicken thighs whole for now. Note 2.",
+      "When roux is the desired color, carefully add your holy trinity (diced vegetables) and garlic and stir vigorously. Alternatively a frozen store mix works fine. It's for flavor and thickening not for texture of the vegetables.",
+      "After 2-3 minutes, or once you notice the vegetables soften slowly add the chicken stock around a cup at a time, allowing the mixture to come to a boil between additions. Note 3.",
+      "Chop the chicken thighs and add them along with the sausage to your gumbo. Leave on medium low heat for at least thirty minutes, but preferably a few hours. DO NOT let the gumbo get above a bare simmer or you will end up with rubbery, dry sausage.",
+      "Season to taste. I like to start with the Tony's until it is most of the way there, then make small adjustments to heat and salinity with the others. If your nose doesn't run, it's not spicy enough.",
+      "Serve with rice and plenty of hot sauce. Garlic bread is also a great addition.",
+    ],
+    imgUrl: "BE/db/photos/gumbo-pic.jpg",
+    notes: [
+      'You should aim for what\'s called a "red roux" which is just past a dark dirt brown. This step takes practice to get right.',
+      "You can alternatively brown these before you start the roux in the same pot/dutch oven, but you have to be careful not to burn the fond when making the roux.",
+      "Homemade stock is best, but Better than Bouillon Roasted Chicken Base works better than any carton stock. Just reconstitute according to package directions.",
+    ],
+  },
+  {
+    title: "Buffalo Chicken Sandwich",
+    estimatedTime: "1 hour",
+    ingredients: [
+      "4 Chicken Thighs",
+      "1 cup Flour",
+      "1 Egg",
+      "1 cup Buttermilk",
+      "1 package of Panko Breadcrumbs",
+      "Spices of your own choice",
+      "Peanut oil for frying",
+      "1 cup Frank's Red Hot",
+      "2-4 tbsp Butter",
+      "1 dash Worcestershire Sauce",
+      "1 dash Honey",
+      "Blue Cheese Dressing to Taste",
+      "Pickled Peppers and Onions to Taste",
+      "4 Brioche Buns",
+    ],
+    content: [
+      "Mix the buttermilk, egg, and spices of your own choice into a large bowl and add chicken thighs to marinate. Leave for at least 30 minutes, but up to overnight.",
+      "While the chicken is marinating, mix the flour with spices in a separate bowl. Set up a third bowl with your panko.",
+      "Heat your oil to 350°F and move your chicken fries from the buttermilk, into the flour, back into the buttermilk, and finally into the panko.",
+      "When your oil is ready, add your chicken to the oil and fry to golden brown.",
+      "While your chicken is cooking, add the Frank's Red Hot and butter to a separate pan to melt the butter. Stir in the honey and worcestershire sauce.",
+      "Once your chicken is cooked, place on a paper towel lined plate or a wire rack while you toast your brioche buns.",
+      "Once your buns are toasted coat your chicken in the sauce and quickly assemble your sandwich with the pickled onions and peppers and blue cheese sauce.",
+      "Enjoy with french fries.",
+    ],
+    imgUrl: "BE/db/photos/buffalo-chicken-pic.JPG",
+    notes: [],
+  },
+  {
+    title: "Blue Cheese Dressing",
+    ingredients: [
+      "1 cup Mayonnaise",
+      "1/2 cup Sour Cream",
+      "1/2 cup Buttermilk",
+      "4 oz Blue Cheese",
+      "1 tbsp White Wine or Apple Cider Vinegar",
+      "1 clove Garlic, Minced",
+      "1/2 tsp of Worcestershire Sauce",
+      "Your Choice of Fresh Herbs like Dill, Rosemary, Oregano, or Thyme",
+      "Salt and Black Pepper to Taste",
+    ],
+    content: [
+      "Add the mayonnaise, sour cream, buttermilk, vinegar, and worcestershire sauce to a bowl and mix until well combined.",
+      "Crumble in the Blue Cheese and add your choice of fresh herbs and the minced garlic.",
+      "Season with the Salt and Black pepper to taste.",
+      "For best results, chill before serving.",
+    ],
+    imgUrl:
+      "https://img.freepik.com/free-photo/top-view-yogurt-meal-brown-wooden-desk-food-yogurt-meat_140725-28373.jpg?w=740&t=st=1707958620~exp=1707959220~hmac=ccb6ad132464b618af23d47c6bbd510185020066158580387806fbb896d3c72c",
+    notes: [
+      "Pick a really good blue cheese for this and you'd be amazed how excellent this can be.",
+    ],
+  },
+  {
+    title: "Quick Pickled Vegetables",
+    ingredients: [
+      "Enough of Any Vegetables of Your Choice to Fill up a Quart Jar",
+      "Any Type of Vinegar",
+      "Plain Water",
+      "Salt to Taste",
+      "1 tsp Sweetener of Your Choice",
+    ],
+    content: [
+      "Chop your vegetables and add to your jar.",
+      "Add enough vinegar to fill the jar halfway and fill the remaining space with water.",
+      "Remove all of this liquid to a pot leaving the vegetables in the jar.",
+      "Heat the mixture to a low boil and add your salt, sweetener, and any desired spices.",
+      "As soon as everything is incorporated, pour the hot liquid over the vegetables in the jar.",
+      "Place a loose lid over the jar and allow to come down to room temperature.",
+      "Place the jar in the fridge.",
+    ],
+    imgUrl:
+      "https://img.freepik.com/free-photo/composition-with-preserved-vegetables_23-2148626045.jpg?w=740&t=st=1707959071~exp=1707959671~hmac=1b45109139179b27b920f254f389123d717756d0e6ee900b66b7ea8f31bb041d",
+    notes: [
+      "This is good with any vegetable, but particularly with peppers and/or onions.",
+      "You can use plain sugar, honey, or any other sweetener. Vary the amount too depending on the vegetables you are using. I like more sweetness with peppers for example.",
+    ],
+  },
+];
 
-module.exports(recipes)
+function concatRecipes(recipes) {
+  return recipes.reduce((acc, recipe) => {
+    return acc.concat("/n", recipe);
+  });
+}
+
+const recipeString = concatRecipes(content);
+function splitRecipe(recipes) {
+  return recipes.split("/n");
+}
+
+module.exports(recipes, concatRecipes, splitRecipe);
