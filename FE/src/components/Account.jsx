450acf31721adb4f4d6ce6f450be97fd81d1c1b4
Lisa Barry <lisa.barry3@gmail.com>
2024-02-17 14:43:24 -0600

    passed setToken into Account.jsx

--- FE/src/components/Account.jsx
+++ FE/src/components/Account.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 
-export default function Account (token) {
+export default function Account (token, setToken) {
 
     return (
         <>This is the Account page</>
