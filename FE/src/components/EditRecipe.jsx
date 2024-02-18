d20692189600f5da62bcb0e3b59a5f0978d3aa45
Lisa Barry <lisa.barry3@gmail.com>
2024-02-17 21:40:35 -0600

    Corrected spelling of recipe

--- FE/src/components/EditRecipe.jsx
+++ FE/src/components/EditRecipe.jsx
@@ -3,6 +3,6 @@ import { Link } from 'react-router-dom'
 export default function EditRecipe ({token}) {
 
     return (
-        <>This is the Edit Recipe page. Should basically be designed to look like the New Reciope page, but with all the recipeInfo preloaded for PATCH</>
+        <>This is the Edit Recipe page. Should basically be designed to look like the New Recipe page, but with all the recipeInfo preloaded for PATCH</>
     )
 }
