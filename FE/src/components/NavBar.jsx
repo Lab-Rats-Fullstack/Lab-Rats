14bb72b68c0a572c653180ec5857e55aa9e17533
Allison Meadors <allisonmeadors@allisons-mbp.mynetworksettings.com>
2024-02-20 19:05:52 -0600

    Added NavBar placeholder
    NavBar file renders Home and Recipes links, and conditionally renders
    link to Account & Admin for admin, Account for user, and Login/Register
    when neither is true.

--- FE/src/components/NavBar.jsx
+++ FE/src/components/NavBar.jsx
@@ -1,8 +1,34 @@
 import { Link } from 'react-router-dom'
 
-export default function NavBar ({token}) {
+export default function NavBar () {
+// will need to pass token, admin as props and as args to renderUserNav() when functionality is available
 
+    function renderUserNav () {
+        let admin = false;
+        let token = false;
+ 
+ 
+        if (admin && token) {
+            return <div>
+            <Link to="/account">Account</Link>
+            <Link to="/admin">Admin</Link>
+            </div>
+        } else if (!admin && token) {
+            return <div>
+                <Link to ="/account">Account</Link>
+            </div>
+        } else {
+            return <div>
+                <Link to="/login">Login or Register</Link>
+            </div>
+        };
+    };
+ 
     return (
-        <><p>This is the NavBar</p></>
-    )
+        <div className="navbar">
+           <Link to="/">Home</Link>
+           <Link to="/recipes">Recipes</Link>
+           {renderUserNav()}
+       </div>
+    );
 }
