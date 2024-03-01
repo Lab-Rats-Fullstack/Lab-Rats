5668ccc82c941e3d262b1bbe531f3eeda073d116
Jared May <jared23m@gmail.com>
2024-02-29 18:42:52 -0600

    fix the username bug

--- BE/api/users.js
+++ BE/api/users.js
@@ -66,7 +66,11 @@ usersRouter.post("/login", async (req, res, next) => {
   }
   try {
     const user = await getUserByUsername(username);
-    const auth = await bcrypt.compare(password, user.password);
+    console.log("user", user);
+    let auth;
+    if (user){
+      auth = await bcrypt.compare(password, user.password);
+    } 
     if (user && auth) {
       const token = jwt.sign(
         { id: user.id, username },
