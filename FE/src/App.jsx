493402e8b217904ca41e232c5a12ee36fc067a4c
Larry Reaux <larryreaux@gmail.com>
2024-03-01 19:59:47 -0600

    finished navbar

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import { Routes, Route} from 'react-router-dom'
-import NavBar from './components/NavBar'
-import MainPage from './components/MainPage'
-import Recipes from './components/Recipes'
-import SingleRecipe from './components/SingleRecipe'
-import NewRecipe from './components/NewRecipe'
-import EditRecipe from './components/EditRecipe'
-import Login from './components/Login'
-import Account from './components/Account'
-import Admin from './components/Admin'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import MainPage from "./components/MainPage";
+import Recipes from "./components/Recipes";
+import SingleRecipe from "./components/SingleRecipe";
+import NewRecipe from "./components/NewRecipe";
+import EditRecipe from "./components/EditRecipe";
+import Login from "./components/Login";
+import Account from "./components/Account";
+import Admin from "./components/Admin";
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useState(null);
 
   return (
-    <><h1>Hello Lab Rats</h1>
-    <NavBar token= {token} setToken={setToken}/>
-    <Routes>
-      <Route path ="/" element={<MainPage token ={token}/>}></Route>
-      <Route path ="/recipes" element={<Recipes token ={token}/>}></Route>
-      <Route path ="/recipes/:recipeId" element={<SingleRecipe token ={token}/>}></Route>
-      <Route path ="/recipes/:recipeId/edit" element={<EditRecipe token ={token}/>}></Route>
-      <Route path ="/recipes/new" element={<NewRecipe token ={token}/>}></Route>
-      <Route path ="/login" element={<Login token ={token} setToken ={setToken}/>}></Route>
-      <Route path ="/account" element={<Account token ={token}/>}></Route>
-      <Route path ="/admin" element={<Admin token ={token}/>}></Route>
-    </Routes>
+    <>
+      <NavBar token={token} setToken={setToken} />
+      <Routes>
+        <Route path="/" element={<MainPage token={token} />}></Route>
+        <Route path="/recipes" element={<Recipes token={token} />}></Route>
+        <Route
+          path="/recipes/:recipeId"
+          element={<SingleRecipe token={token} />}
+        ></Route>
+        <Route
+          path="/recipes/:recipeId/edit"
+          element={<EditRecipe token={token} />}
+        ></Route>
+        <Route
+          path="/recipes/new"
+          element={<NewRecipe token={token} />}
+        ></Route>
+        <Route
+          path="/login"
+          element={<Login token={token} setToken={setToken} />}
+        ></Route>
+        <Route path="/account" element={<Account token={token} />}></Route>
+        <Route path="/admin" element={<Admin token={token} />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
