666b4a6a9de11335d0174765eccca6d2772c72e9
Lisa Barry <lisa.barry3@gmail.com>
2024-03-02 23:51:53 -0600

    added token conditional on certain info

--- FE/src/components/UserInfo.jsx
+++ FE/src/components/UserInfo.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-export default function UserInfo ({adminPriv, userData}) {
+export default function UserInfo ({token, adminPriv, userData}) {
     // console.log(userData);
     // console.log(adminPriv);
     return (
         <div className = 'userInfo'>
             <img src={userData.imgurl} alt={`User account image for ${userData.username}`} height="15%" width="22.5%"/>
             <h2>{userData.username}</h2>
-            <p>Name: {userData.name}</p>
-            <p>Email: {userData.email}</p>
+            {token !== null && 
+            <div className= 'userPersonal'>
+                <p>Name: {userData.name}</p>
+                <p>Email: {userData.email}</p>
+            </div>
+            }
             {adminPriv && <p>{userData.admin ? "Admin Status: Admin" : "Admin Status: User"}</p>}
         </div>
     )
