d6fc200a718ddd803fa2d4b8bc458f5c28c4dd42
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-03-05 18:38:05 -0600

    New styles (#114)
    * added new styling info
    
    * finished adding styling
    
    * made navbar sticky, edited admin page styles, added global image styles.
    
    * admin now gets private info from get /:userId, regular users and lurkers only get public info (#110)
    
    * added auth to user get to distinguish admin
    
    * fixed middleware issue
    
    * name and email no longer publicly available
    
    * changed relation reference comment to comments
    
    * 93 single recipe connect to BE (#106)
    
    * added reviews and comments, no listeners
    
    * 93-single-recipe-connect-to-BE
    
    * added more comment state stuff
    
    * added userId to response data
    
    * create reviews and comments working
    
    * added edit review
    
    * reset states when new forms pop up
    
    * Added edit comment and navigates
    
    * added deleteReview
    
    * edit and delete final touches, only admin features left to go
    
    * finished admin features
    
    * user can no longer edit admin status
    
    * removed img width inline, made API_URL const
    
    * redoing fe autho
    
    * auth conditional working
    
    * admin stuff including BE fully working
    
    * front end auth working
    
    * alreadyReviewed auth works on FE and BE
    
    * avgRating fully working and present everywhere needed on BE
    
    * avgRating rounds to tenths now, also displays on SingleRecipe
    
    * changed null to 0 if ratings.length == 0
    
    * finished initial styling
    
    ---------
    
    Co-authored-by: jared23m <45523770+jared23m@users.noreply.github.com>

--- FE/src/components/RecipeCard.jsx
+++ FE/src/components/RecipeCard.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import TagInfo from './TagInfo';
-import NavButton from './NavButton';
+import React from "react";
+import TagInfo from "./TagInfo";
+import NavButton from "./NavButton";
+import defaultImg from "../assets/default food.jpeg";
 
-export default function RecipeInfo ({recipe, admin, currentUser}) {
-
-    return (
-        <div className="recipeCard">
-            <img src={recipe.imgurl} alt={`A picture of ${recipe.title}`}/>
-            <p className ="recipeTitle">{recipe.title}</p>
-            <p className="recipeUsername">{recipe.username}</p>
-            {recipe.tags.map((tag)=> {         
-                return (
-                    <TagInfo key={tag.id} tag = {tag} />
-                )
-            })}
-            <p>Est. Time: {recipe.estimatedtime}</p>
-            <NavButton location ={`/recipes/${recipe.id}`} buttonText={"See Recipe"}/>
-            <NavButton location ={`/recipes/${recipe.id}/edit`} buttonText={"Edit Recipe"}/>
-            {/* need to make this conditional on being admin and tier 2 currentUser if username matches */}
-        </div>
-    )
+export default function RecipeInfo({ recipe, admin, currentUser }) {
+  return (
+    <div className="recipeCard">
+      <img
+        src={recipe.imgurl || defaultImg}
+        alt={`A picture of ${recipe.title}`}
+      />
+      <p className="recipeTitle">{recipe.title}</p>
+      <p className="recipeUsername">{recipe.username}</p>
+      {recipe.tags.map((tag) => {
+        return <TagInfo key={tag.id} tag={tag} />;
+      })}
+      <p>Est. Time: {recipe.estimatedtime}</p>
+      <NavButton location={`/recipes/${recipe.id}`} buttonText={"See Recipe"} />
+      <NavButton
+        location={`/recipes/${recipe.id}/edit`}
+        buttonText={"Edit Recipe"}
+      />
+      {/* need to make this conditional on being admin and tier 2 currentUser if username matches */}
+    </div>
+  );
 }
