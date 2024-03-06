4ba8225214dfef67c115657344d9264b365d9e3a
Allison Meadors <allisonmeadors@allisons-mbp.mynetworksettings.com>
2024-03-05 19:00:01 -0600

    Fixed get call
    Featured Recipe & tags are now functional

--- FE/src/components/MainPage.jsx
+++ FE/src/components/MainPage.jsx
@@ -1,5 +1,6 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import RecipeCard from './RecipeCard'
 
 export default function MainPage ({token}) {
     const [recipes, setRecipes] = useState([]);
@@ -14,7 +15,7 @@ export default function MainPage ({token}) {
             try {
                 const response = await fetch(`${API}/recipes/`);
                 const result = await response.json();
-                setRecipes(result.recipes);
+                return result;
             } catch(error) {
                 console.error(error);
             }
@@ -23,119 +24,12 @@ export default function MainPage ({token}) {
         async function setState() {
             const someVar = await getRecipes();
             if(someVar) {
-                setRecipes(someVar.recipes);
+                setRecipes(someVar);
             }
         }
 
         setState();
     }, []);
-
-    useEffect(() => {
-        console.log(recipes);
-    }, [recipes])
-
-
-    const dummyRecipes = [
-        {
-            id: 3,
-            userId: 3,
-            userName: "kiss_the_chef",
-            title: "Butter Chicken",
-            estimatedTime: "1 hour",
-            ingredients: [
-              "1.5 lbs Chicken Thighs",
-              "1 cup Full-Fat Yogurt",
-              "1 tbsp Grated or Crushed Garlic",
-              "1 tbsp Grated Ginger",
-              "1 tsp Turmeric Powder",
-              "1 tsp Ground Cumin",
-              "1 tsp Ground Coriander",
-              "1 tsp Garam Masala",
-              "Salt to Taste",
-              "1 tsp Oil or Ghee",
-              "2 tbsp Butter",
-              "1 Large Onion, Finely Diced",
-              "1 cup Tomato Puree",
-              "1 cup Heavy Cream",
-              "Fresh Cilantro, for Garnish",
-            ],
-            procedure: [
-              "Mix yogurt, ginger, garlic, and spices (reserve half of garam masala) to a large bowl and mix well. Add in the chicken and marinate for at least 30min up to a day.",
-              "In a large pan or dutch oven, add your oil over medium heat. Cook the chicken until well browned. It does not need to be cooked through at this stage.",
-              "Set the chicken aside and add the butter to the pan. Once melted and bubbling, add the chopped onion to the pan.",
-              "Once the onions are fully softened, add in the tomato puree and cook on high until the oil separated and the tomato starts to brown.",
-              "Add the chicken back to the pan and stir to combine the mixture.",
-              "Pour in the heavy cream and cook on medium low heat for 10-15 minutes or until the chicken is tender.",
-              "Add in your remaining garam masala and any additional salt to taste. Adjust the consistency of the gravy with water only if absolutely necessary.",
-              "Serve with naan and rice. Garnish with your chopped cilantro.",
-            ],
-            imgUrl:
-              "https://www.foodiesfeed.com/wp-content/uploads/2023/03/close-up-of-butter-chicken-indian-dish.jpg",
-            notes: [
-              "I like to make the naan too as its a very easy and approachable flat bread to make at home.",
-            ],
-            tags: ["gluten free"],
-        },
-        {
-            id: 4,
-            userId: 4,
-            userName: "bleu_cheese_lover",
-            title: "Blue Cheese Dressing",
-            estimatedTime: "45 min",
-            ingredients: [
-              "1 cup Mayonnaise",
-              "1/2 cup Sour Cream",
-              "1/2 cup Buttermilk",
-              "4 oz Blue Cheese",
-              "1 tbsp White Wine or Apple Cider Vinegar",
-              "1 clove Garlic, Minced",
-              "1/2 tsp of Worcestershire Sauce",
-              "Your Choice of Fresh Herbs like Dill, Rosemary, Oregano, or Thyme",
-              "Salt and Black Pepper to Taste",
-            ],
-            procedure: [
-              "Add the mayonnaise, sour cream, buttermilk, vinegar, and worcestershire sauce to a bowl and mix until well combined.",
-              "Crumble in the Blue Cheese and add your choice of fresh herbs and the minced garlic.",
-              "Season with the Salt and Black pepper to taste.",
-              "For best results, chill before serving.",
-            ],
-            imgUrl:
-              "https://img.freepik.com/free-photo/top-view-yogurt-meal-brown-wooden-desk-food-yogurt-meat_140725-28373.jpg?w=740&t=st=1707958620~exp=1707959220~hmac=ccb6ad132464b618af23d47c6bbd510185020066158580387806fbb896d3c72c",
-            notes: [
-              "Pick a really good blue cheese for this and you'd be amazed how excellent this can be.",
-            ],
-            tags: ["condiment"],
-          },
-          {
-            id: 5,
-            userId: 5,
-            userName: "pickle_boy",
-            title: "Quick Pickled Vegetables",
-            estimatedTime: "1 hour",
-            ingredients: [
-              "Enough of Any Vegetables of Your Choice to Fill up a Quart Jar",
-              "Any Type of Vinegar",
-              "Plain Water",
-              "Salt to Taste",
-              "1 tsp Sweetener of Your Choice",
-            ],
-            procedure: [
-              "Chop your vegetables and add to your jar.",
-              "Add enough vinegar to fill the jar halfway and fill the remaining space with water.",
-              "Remove all of this liquid to a pot leaving the vegetables in the jar.",
-              "Heat the mixture to a low boil and add your salt, sweetener, and any desired spices.",
-              "As soon as everything is incorporated, pour the hot liquid over the vegetables in the jar.",
-              "Place a loose lid over the jar and allow to come down to room temperature.",
-              "Place the jar in the fridge.",
-            ],
-            imgUrl:
-              "https://img.freepik.com/free-photo/composition-with-preserved-vegetables_23-2148626045.jpg?w=740&t=st=1707959071~exp=1707959671~hmac=1b45109139179b27b920f254f389123d717756d0e6ee900b66b7ea8f31bb041d",
-            notes: [
-              "This is good with any vegetable, but particularly with peppers and/or onions.",
-              "You can use plain sugar, honey, or any other sweetener. Vary the amount too depending on the vegetables you are using. I like more sweetness with peppers for example.",
-            ],
-            tags: ["vegetarian"]
-          }]
 
     useEffect(() => {
         function selectRecipe(array) {
@@ -144,9 +38,8 @@ export default function MainPage ({token}) {
             return randRecipe;
         }
     
-        let randRecipeState = selectRecipe(dummyRecipes);
+        let randRecipeState = selectRecipe(recipes);
         setFeatRecipe(randRecipeState)
-        console.log(featRecipe);
     }, [recipes]);
 
     useEffect(() => {
@@ -160,7 +53,6 @@ export default function MainPage ({token}) {
             }
         }
         getTags();
-        console.log(tags);
     }, []);
 
     useEffect(() => {
@@ -174,28 +66,19 @@ export default function MainPage ({token}) {
 
         let rearrTags = rearrange(tags, 'name');
         setTagsOutput(rearrTags);
-        console.log(tagsOutput);
     }, [tags]);
 
     function handleTagsClick(e) {
         e.preventDefault();
         let tagFromButton = e.target.value;
-        console.log(e.target.value);
         navigate(`/tags/${tagFromButton}/recipes`);
     };
 
     return (
         <>
-        <div className="featRecipe">
-        <h2>{featRecipe.title}</h2>
-                       <h4>{featRecipe.userName}</h4>
-                       <img src={featRecipe.imgUrl} alt={`A picture of ${featRecipe.title}`} height="15%" width="22.5%"/>
-                       <p><em>{featRecipe.tags}</em></p>
-                       <p>Est. Time: {featRecipe.estimatedTime}</p>
-                       <button onClick={() => {
-                           navigate(`/recipes/${featRecipe.id}`)
-                       }}>See Recipe</button>
-        </div>
+        {recipes && featRecipe && featRecipe.tags ? <RecipeCard key={featRecipe.id} recipe={featRecipe}/>
+         : <div>error</div>}
+            
         <div className="mainTags">
             {tagsOutput.map((tag) => {
                 return (
--- FE/src/components/RecipesByTag.jsx
+++ FE/src/components/RecipesByTag.jsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import RecipeCard from './RecipeCard'
 
 export default function renderRecipesByTag() {
     const API = "http://localhost:3000/api";
     const [recipeList, setRecipeList] = useState([]);
     const { tagname } = useParams();
 
-    console.log(tagname);
     useEffect(() => {
         async function getRecipesByTag() {
             try {
                 const response = await fetch(`${API}/tags/` + tagname + `/recipes`);
                 const result = await response.json();
-                setRecipeList(result.recipes);
+                setRecipeList(result);
             } catch(error) {
                 console.error(error);
             }
@@ -22,20 +22,10 @@ export default function renderRecipesByTag() {
 
     return (
         <div>
-            <p>hello from { tagname } recipes</p>
-            {/* {recipeList.map((recipe) => {
+             {recipeList.map((recipe) => {
                return (
-                   <div className="recipeCard" key={recipe.id}>
-                       <h2>{recipe.title}</h2>
-                       <h4>{recipe.userName}</h4>
-                       <img src={recipe.imgUrl} alt={`A picture of ${recipe.title}`} height="15%" width="22.5%"/>
-                       <p><em>{recipe.tags}</em></p>
-                       <p>Est. Time: {recipe.estimatedTime}</p>
-                       <button onClick={() => {
-                           navigate(`/recipes/${recipe.id}`)
-                       }}>See Recipe</button>
-                   </div>
-               )})};  commented out bc it doesn't work without get call*/}
+                   <RecipeCard key={recipe.id} recipe={recipe}/>
+               )})};
         </div>
     )
 }
