16e66f1f832f5be23600286e3aa0fe436fede8a2
Larry Reaux <larryreaux@gmail.com>
2024-03-07 17:31:20 -0600

    finished picture functionality

--- FE/src/components/UploadImage.jsx
+++ FE/src/components/UploadImage.jsx
@@ -1,16 +1,24 @@
 export default function UploadImage({ setEncoded }) {
   function handleChange(e) {
     const encode = encodeImageFileAsURL(e.target.files[0]);
-    console.log(encode)
+    console.log(encode);
   }
 
   function encodeImageFileAsURL(file) {
     const reader = new FileReader();
     reader.onloadend = function () {
-      setEncoded({base64: reader.result});
+      setEncoded({ base64: reader.result });
     };
     reader.readAsDataURL(file);
   }
 
-  return <input type="file" name="image" id="image" accept="image/*" onChange={handleChange} />;
+  return (
+    <input
+      type="file"
+      name="image"
+      id="image"
+      accept="image/*"
+      onChange={handleChange}
+    />
+  );
 }
