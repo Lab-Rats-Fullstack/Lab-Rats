659597c47f5781bd71038cc5bfe677dddee00d64
Larry Reaux <larryreaux@gmail.com>
2024-03-06 21:47:35 -0600

    finished api documentation, fixed bug with endpoint

--- BE/api/reviews.js
+++ BE/api/reviews.js
@@ -47,7 +47,7 @@ reviewRouter.patch("/:reviewId", requireUser, async (req, res, next) => {
   }
 });
 
-reviewRouter.delete("/:reviewId", requireUser, async (req, res) => {
+reviewRouter.delete("/:reviewId", requireUser, async (req, res, next) => {
   const {
     params: { reviewId },
     user: { id },
