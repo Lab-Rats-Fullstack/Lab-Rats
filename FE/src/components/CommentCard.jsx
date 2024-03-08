7226ba1945a4f78dd2b6100cbb99f8295bd8227f
BarryFullStack <146087160+BarryFullStack@users.noreply.github.com>
2024-03-07 19:02:25 -0600

    Account PATCH bug and comments display fixed (#123)
    * Account PATCH bug and comments display fixed
    Added a cancel button for form, if password is "" and updatedUser is null form will cancel, password will not push to API if "". password and confirmPassword states are reset to "" after user form has submitted.
    Comments just had a data pathing issue.
    
    * actually resolved the form api patch bug
    
    * Removed admin form condition
    Admin update should on;y apply to the usrProfile page form to prevent admin from removing their own status and thus a lockout.

--- FE/src/components/CommentCard.jsx
+++ FE/src/components/CommentCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import NavButton from './NavButton';
+import NavButton from "./NavButton";
 
-export default function ReviewInfo ({comment}) {
-return (
-    <div className="commentCard" key={comment.id}>
-        <p className="commentContent">Comment: {comment.content}</p>
-        <p>On review {comment.review.title} for recipe {comment.recipe.title}</p>
-        <NavButton location ={`/recipes/${comment.recipe.id}`} buttonText={"See Recipe"}/>
-    </div>)
+export default function CommentInfo ({comment}) {
+
+    return (
+        <div className="commentCard" key={comment.id}>
+            <p className="commentContent">Comment: {comment.content}</p>
+            <p>On review {comment.review.title} for recipe {comment.review.recipe.title}</p>
+            <NavButton location ={`/recipes/${comment.review.recipe.id}`} buttonText={"See Recipe"}/>
+        </div>)
 }
