a49f3a3a3eb76446e4dc024ab8ab7eaff5f956bb
Jared May <jared23m@gmail.com>
2024-03-07 19:35:40 -0600

    added account user links on reviews and comments

--- FE/src/components/CommentCard.jsx
+++ FE/src/components/CommentCard.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import NavButton from "./NavButton";
+import {Link} from "react-router-dom";
 
-export default function CommentInfo ({comment}) {
+export default function CommentInfo ({comment, currentUser}) {
 
     return (
         <div className="commentCard" key={comment.id}>
             <p className="commentContent">Comment: {comment.content}</p>
-            <p>On review {comment.review.title} for recipe {comment.review.recipe.title}</p>
+            <p>On review {comment.review.title}</p>
+            <p>by {(comment.review.user.username === currentUser) ?
+                    <Link className="username"to={`/account`}>@{comment.review.user.username}</Link>
+                     :
+                    <Link className="username"to={`/users/${comment.review.user.id}`}>@{comment.review.user.username}</Link>
+                    }</p>
+            <p>for recipe {comment.review.recipe.title}</p>
+            <p>by {(comment.review.recipe.user.username === currentUser) ?
+                    <Link className="username"to={`/account`}>@{comment.review.recipe.user.username}</Link>
+                     :
+                    <Link className="username"to={`/users/${comment.review.recipe.user.id}`}>@{comment.review.recipe.user.username}</Link>
+                    }</p>
             <NavButton location ={`/recipes/${comment.review.recipe.id}`} buttonText={"See Recipe"}/>
         </div>)
 }
