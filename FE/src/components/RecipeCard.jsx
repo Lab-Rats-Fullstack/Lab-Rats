3b80c7a3512775ce2d1b89f6653b7a840b1789c6
amead99 <124738520+amead99@users.noreply.github.com>
2024-03-07 18:53:13 -0600

    All Recipes get (#139)
    * All Recipes get
    
    Recipes.jsx makes real get call. Chnaged rendering of the search bar
    so that it would work together with the all recipes list. Changed edit
    button on recipe card to render for admin only.
    
    * Added updated app.jsx with props
    
    * Fixed recipe card username
    
    * Search Bar patch
    
    ---------
    
    Co-authored-by: Allison Meadors <allisonmeadors@allisons-mbp.mynetworksettings.com>

--- FE/src/components/RecipeCard.jsx
+++ FE/src/components/RecipeCard.jsx
@@ -3,7 +3,8 @@ import TagInfo from "./TagInfo";
 import NavButton from "./NavButton";
 import defaultImg from "../assets/default food.jpeg";
 
-export default function RecipeInfo({ recipe, admin, currentUser }) {
+export default function RecipeInfo({ recipe, admin, currentUser, token}) {
+
   return (
     <div className="recipeCard">
       <img
@@ -17,10 +18,10 @@ export default function RecipeInfo({ recipe, admin, currentUser }) {
       })}
       <p>Est. Time: {recipe.estimatedtime}</p>
       <NavButton location={`/recipes/${recipe.id}`} buttonText={"See Recipe"} />
-      <NavButton
+      {admin ? (<NavButton
         location={`/recipes/${recipe.id}/edit`}
         buttonText={"Edit Recipe"}
-      />
+      />) : (<></>)}
       {/* need to make this conditional on being admin and tier 2 currentUser if username matches */}
     </div>
   );
