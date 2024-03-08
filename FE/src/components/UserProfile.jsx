7a80582e82da93d81ee6136b1ef33c7528954537
Lisa Barry <lisa.barry3@gmail.com>
2024-03-07 20:18:37 -0600

    adding form code

--- FE/src/components/UserProfile.jsx
+++ FE/src/components/UserProfile.jsx
@@ -16,17 +16,12 @@ export default function NewRecipe ({token, admin, currentUser}) {
     const [error, setError] = useState(null);
     const navigate = useNavigate();
     const { userId } = useParams();
+    console.log(userId);
 
     useEffect(()=>{
         async function userCheck () {
             try {
-                const response = await fetch(`${API}users/${userId}`, {
-                    method: "GET",
-                    headers: {
-                        "Content-Type":"application/json",
-                        // "Authorization": `Bearer ${token}`,
-                    },
-                });
+                const response = await fetch(`${API}users/${userId}`);
                 const result = await response.json();
                 console.log(result);
                 setUserData(result);
@@ -42,14 +37,95 @@ export default function NewRecipe ({token, admin, currentUser}) {
         <div className = 'wrapper'>
             {/* {error && <p>{error}</p>} */}
             {error ? <div className="error"><p>{error}</p></div> :
-            <div>
-                <div>
+            <div className="userInfoContainer">
+                <div className="userInfo">
                     <UserInfo
                         key ={userData.id}
                         token={token}
                         userData={userData}
                         admin={admin}/>
                 </div>
+
+                {userForm &&
+                <div className = 'userUpdateForm'>
+                    <form onSubmit = {userUpdate}>
+                        <label>
+                            Profile Image:
+                            <input
+                                defaultValue = {userData.imgurl}
+                                onChange = {(e)=> setUpdatedUser((prev)=>{
+                                    return {
+                                    ...prev,
+                                        imgurl: e.target.value
+                                    }
+                                })}
+                            />
+                        </label>
+                        <label>
+                            Username:
+                            <input
+                                defaultValue = {userData.username}
+                                onChange = {(e)=> setUpdatedUser((prev)=>{
+                                    return {
+                                    ...prev,
+                                        username: e.target.value
+                                    }
+                                })}
+                            />
+                        </label>
+                        <label>
+                            Email:
+                            <input
+                                defaultValue = {userData.email}
+                                onChange = {(e)=> setUpdatedUser((prev)=>{
+                                    return {
+                                    ...prev,
+                                        email: e.target.value
+                                    }
+                                })}
+                            />
+                        </label>
+                        <label>
+                            Name:
+                            <input
+                                defaultValue = {userData.name}
+                                onChange = {(e)=> setUpdatedUser((prev)=>{
+                                    return {
+                                    ...prev,
+                                        name: e.target.value
+                                }})}
+                            />
+                        </label>
+                        <label>
+                            Password:
+                            <input
+                                type = "password"
+                                value = {password}
+                                autoComplete='off'
+                                onChange = {(e)=> {setPassword(e.target.value);
+                                    setUpdatedPassword((prev)=>{
+                                    return {
+                                    ...prev,
+                                        password: e.target.value
+                                }})}}
+                            />
+                        </label>
+                        <label>
+                            Confirm Password:
+                            <input
+                                type = "password"
+                                value = {confirmPassword}
+                                autoComplete='off'
+                                onChange = {(e)=> setConfirmPassword(e.target.value)}
+                            />
+                        </label>
+                        {buttonStatus == true && <p>Passwords must match</p>}
+                        <button type='submit' disabled = {buttonStatus} >Submit</button>
+                        <button onClick={() => {setUserBio(true); setUserForm(false);}} >Cancel</button>
+                    </form>
+                </div>
+                }
+
                 <div className = 'userItems'>
                     <div className='userItemsNav'>
                         <NavButton location ={`/users/${userId}/recipes`} buttonText={"My Recipes"}/>
