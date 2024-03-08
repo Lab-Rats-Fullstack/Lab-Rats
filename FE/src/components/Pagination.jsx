938dd95c50bcbe249c8852c7e99b226fb55c5b12
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-03-08 20:16:58 +0000

    Compled Pagination component

---
+++ FE/src/components/Pagination.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+import RecipeInfo from "./RecipeCard";
+
+export default function Pagination({recipeList, currentRecipes, setCurrentRecipes, numberPerPage, admin, currentUser, token }){
+    const [numberOfPages, setNumberofPages] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+
+   function determineNumberOfPages(arr){
+    return Math.ceil(arr.length/numberPerPage)
+   }
+
+   function splitRecipesIntoPages(){
+    const pages = determineNumberOfPages(recipeList)
+    setNumberofPages(pages)
+   }
+
+   function sliceRecipes(arr){
+    const start = (currentPage - 1)*numberPerPage
+    const end = start + numberPerPage
+    console.log(start, end)
+    const currentPageRecipes = arr.slice(start, end)
+    setCurrentRecipes(currentPageRecipes)
+   }
+
+   function CreatePageButtons() {
+    const arr = new Array(numberOfPages).fill(0);
+    return(<div>
+        {arr.map((item, idx)=>{
+            return <button onClick={()=> setCurrentPage(idx+1)}>{idx+1}</button>
+        })}
+    </div>)
+   }
+
+   useEffect(()=>{splitRecipesIntoPages()}, [numberOfPages, currentPage])
+   useEffect(() => sliceRecipes(recipeList), [ currentPage])
+
+    return( 
+        <div>
+            <RecipeCard recipe={currentRecipes} admin={admin} currentUser={currentUser} token={token}/>
+            <CreatePageButtons/>
+        </div>)
+}
