a60caf18ec539747bf2a37f727dd426a31a275d4
Larry Reaux <larryreaux@gmail.com>
2024-03-09 09:41:38 -0600

    added long wait time message

--- FE/src/components/Loading.jsx
+++ FE/src/components/Loading.jsx
@@ -1,3 +1,21 @@
+import { useState, useEffect } from "react";
+
 export default function Loading() {
-    return  <div className="container"><p className="loading">Loading</p></div>
+  const [waiting, setWaiting] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setWaiting(true), 10000);
+  }, []);
+  return (
+    <div className="loadingContainer">
+      <p className="loading">Loading</p>
+      {waiting && (
+        <p>
+          Still Waiting? Culinary Chronicle is run on a free database and web
+          service that sometimes involves long wait times. Try refreshing the
+          page now that the service is up and running.
+        </p>
+      )}
+    </div>
+  );
 }
