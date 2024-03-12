a7dc6c91898e01bce13f2d74e7eb3f77db738338
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-03-11 20:10:09 -0500

    fix-review-cards (#177)
    * pause
    
    * fixed comment/review cards

--- FE/src/components/CommentCard.jsx
+++ FE/src/components/CommentCard.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React from "react";
 import NavButton from "./NavButton";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function CommentInfo ({comment, currentUser}) {
-
-    return (
-        <div className="commentCard" key={comment.id}>
-            <p className="commentContent">Comment: {comment.content}</p>
-            <p>On review {comment.review.title}</p>
-            <p>by {(comment.review.user.username === currentUser) ?
-                    <Link className="username"to={`/account`}>@{comment.review.user.username}</Link>
-                     :
-                    <Link className="username"to={`/users/${comment.review.user.id}`}>@{comment.review.user.username}</Link>
-                    }</p>
-            <p>for recipe <Link className="recipeTitle" to={`/recipes/${comment.review.recipe.id}`}>{comment.review.recipe.title}</Link></p>
-            <p>by {(comment.review.recipe.user.username === currentUser) ?
-                    <Link className="username"to={`/account`}>@{comment.review.recipe.user.username}</Link>
-                     :
-                    <Link className="username"to={`/users/${comment.review.recipe.user.id}`}>@{comment.review.recipe.user.username}</Link>
-                    }</p>
-            <NavButton location ={`/recipes/${comment.review.recipe.id}`} buttonText={"See Recipe"}/>
-        </div>)
+export default function CommentInfo({ comment, currentUser }) {
+  return (
+    <div className="commentCard" key={comment.id}>
+      <p className="commentContent">Comment: {comment.content}</p>
+      <p className="reviewName">On review: {comment.review.title}</p>
+      <p className="cardUsername">
+        by{" "}
+        {comment.review.user.username === currentUser ? (
+          <Link to={`/account`}>@{comment.review.user.username}</Link>
+        ) : (
+          <Link to={`/users/${comment.review.user.id}`}>
+            @{comment.review.user.username}
+          </Link>
+        )}
+      </p>
+      <Link
+        className="reviewRecipeTitle"
+        to={`/recipes/${comment.review.recipe.id}`}
+      >
+        {comment.review.recipe.title}
+      </Link>
+      <p className="cardUsername">
+        by{" "}
+        {comment.review.recipe.user.username === currentUser ? (
+          <Link className="username" to={`/account`}>
+            @{comment.review.recipe.user.username}
+          </Link>
+        ) : (
+          <Link
+            className="username"
+            to={`/users/${comment.review.recipe.user.id}`}
+          >
+            @{comment.review.recipe.user.username}
+          </Link>
+        )}
+      </p>
+      <NavButton
+        location={`/recipes/${comment.review.recipe.id}`}
+        buttonText={"See Recipe"}
+      />
+    </div>
+  );
 }
